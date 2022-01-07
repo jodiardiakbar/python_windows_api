b7f5a56aedb3a3e71b6b3e9a821677c3ea892404
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2022-01-07 13:59:09 -0500

    Add files via upload

---
+++ openProcHandle.py
@@ -0,0 +1,28 @@
+import ctypes
+
+# interacting with Processes, File Systems, Threads
+kernel_handle = ctypes.windll("Kernel32.dll")
+
+# process security and access rights
+PROCESS_ALL_ACCESS = (0x000F0000L | 0x00100000L | 0xFFFF)
+
+# parameters for OpenProcess function
+dwDesiredAccess = PROCESS_ALL_ACCESS
+bInheritHandle = False
+
+# grep from window task manager, in hexadecimal
+dwProcessId = int(hex(2100))
+print(f"dwProcessId = {dwProcessId}")
+
+# error handling
+error = kernel_handle.GetLastError()
+if error != 0:
+    print(f"Err Code: {error}")
+    exit(1)
+
+# response result
+response = kernel_handle.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
+if response <= 0:
+    print(f"Handle was not created, response = {response}")
+else:
+    print(f"Handle created !, response = {response}")
